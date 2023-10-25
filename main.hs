2ce16918e1259f04f77fbae9ced41151ae497bf0
dinho11701 <48062027+dinho11701@users.noreply.github.com>
2023-10-25 06:50:42 -0400

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,4 +1,4 @@
-import Move
+{--import Move
 
 
 main :: IO ()
@@ -18,6 +18,45 @@ main = do
   
   let (taille, x, y) = firstDropInfo
   print taille
+
+--}
+import System.IO
+import Move 
+
+-- Fonction pour demander à l'utilisateur de saisir une chaîne de caractères
+promptString :: String -> IO String
+promptString message = do
+    putStr message
+    hFlush stdout
+    getLine
+
+-- Fonction pour saisir les informations de l'utilisateur et les stocker dans un tableau
+getUserInfo :: IO [String]
+getUserInfo = do
+    coup <- promptString "Entrez votre coup : "
+
+    return [coup]
+
+main :: IO ()
+main = do
+    userInfo <- getUserInfo
+    putStrLn "Informations de l'utilisateur :"
+    putStrLn $ "Coup : " ++ head userInfo  -- Utilisez "head" pour accéder au premier élément de la liste
+
+    let parsedMoves = parseMoves userInfo
+    putStrLn "Mouvements analysés :"
+    mapM_ print parsedMoves
+
+    -- Récupération de la taille, x et y du premier drop s'il existe
+    let firstDropInfo = case findDropInfo parsedMoves of
+            Just (size, x, y) -> (size, x, y)
+            Nothing -> (B, -1, -1)  -- Valeurs par défaut si pas de drop
+
+    putStrLn "Info du premier drop :"
+    print firstDropInfo
+  
+    let (taille, x, y) = firstDropInfo
+    print taille
 
 
 
