8869d381ce27304e79e6b8296006ad6289f14955
dinho11701 <48062027+dinho11701@users.noreply.github.com>
2023-11-07 06:02:57 -0800

    Update Move.hs

--- Move.hs
+++ Move.hs
@@ -1,11 +1,27 @@
+
+
+
 module Move where
 
 import Text.Parsec
 import Text.Parsec.String
 
-data Size = T | S | M | B deriving Show
-data Position = Position Int Int deriving Show
-data Move = Drop Size Position | Onboard Position Position deriving Show
+data Size 
+  
+  = T  -- ^ Represente la taille la plus petite, T pour Tiny.
+  | S  -- ^ Represente la taille small S.
+  | M  -- ^ Represents la taille medium M .
+  | B  -- ^ Represents la taille Big. 
+  deriving Show
+  
+-- | Type de donnée pour représenter la position dans un jeu.
+data Position
+  = Position Int Int -- ^ Constructeur de x et y
+  deriving Show
+  
+data Move = Drop Size Position -- ^ Mouvement drop pour placer la piece de a une position
+    | Onboard Position Position -- ^ Mouvement onboard pour déplacer la piece d'une position a une autre
+    deriving Show
 
 -- Analyseur pour les tailles
 sizeParser :: Parser Size
@@ -34,7 +50,7 @@ onboardParser = do
     char '('
     position1 <- positionParser
     char ','
-    spaces
+    optional spaces
     position2 <- positionParser
     char ')'
     return (Onboard position1 position2)
@@ -46,7 +62,7 @@ dropParser = do
     char '('
     size <- sizeParser
     char ','
-    spaces
+    optional spaces
     position <- positionParser
     char ')'
     return (Drop size position)
@@ -59,6 +75,7 @@ parseMoves input =
         Left _ -> []
 
 
+-- | Transforme une taille en string 
 transformeSizeEnString :: Size -> String
 transformeSizeEnString T = "T"
 transformeSizeEnString B = "B"
@@ -66,17 +83,19 @@ transformeSizeEnString S = "S"
 transformeSizeEnString M = "M"
 
 
--- Fonction pour extraire le x, y et la taille du premier drop
+-- | Fonction pour extraire le ou les x, y et la taille dependamment de si cest un drop ou onboard
 recupererDropOuOnboardInfo :: [Move] -> Maybe (Size, Int, Int, Int, Int)
 recupererDropOuOnboardInfo [] = Nothing
 recupererDropOuOnboardInfo (Drop size (Position x y) : _) = Just (size, x, y,-1,-1)
 recupererDropOuOnboardInfo (Onboard (Position x1 y1) (Position x2 y2) : _) = Just (B,x1,y1,x2, y2)
 
 
+-- | Détermine si un coup est valide 
 estUnCoupValide :: Int -> Int -> Bool
 estUnCoupValide x y = x >= 0 && x <= 3 && y >= 0 && y <= 3 
 
 
+-- | Vérifie qu'un coup est valide ou non
 verifierCoup :: [Int] -> Bool
 verifierCoup liste
     | all(\x -> x == -1) liste = False
@@ -84,11 +103,8 @@ verifierCoup liste
     | otherwise = True
     
 
+-- | Détermine si le coup joué est un drop
 estCasDrop :: [Int] -> Bool
 estCasDrop liste = all(\x -> x == -1) liste 
-
-
-
-
 
 
